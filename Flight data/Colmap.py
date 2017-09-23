14dd8604263753400228fa6def01387196e4d683
SoumitraAgarwal <agarwalsoumitra1504@gmail.com>
2017-09-24 02:05:52 +0530

    Final commit

--- Flight data/Colmap.py
+++ Flight data/Colmap.py
@@ -3,10 +3,13 @@ import pandas as pd
 import numpy as np
 import calmap
 
-data = pd.read_csv("Results.csv")
-all_days = pd.date_range('1/1/2015', periods=365, freq='D')
-all_days = np.array(all_days.to_pydatetime(), dtype=np.datetime64)
+data 		= pd.read_csv("Results.csv")
+all_days 	= pd.date_range('1/1/2015', periods=365, freq='D')
+all_days 	= np.array(all_days.to_pydatetime(), dtype=np.datetime64)
 
-events = pd.Series(data["Values"].values, index=all_days)
-calmap.yearplot(events, year=2015, cmap='YlGn')
+events 	= pd.Series(data["Values"].values, index=all_days)
+fig 	= plt.figure(figsize=(20,8))
+ax 		= fig.add_subplot(111)
+cax 	= calmap.yearplot(events, year=2015, ax=ax, cmap='rainbow')
+fig.colorbar(cax.get_children()[1], ax=cax, orientation='horizontal')
 plt.show()
